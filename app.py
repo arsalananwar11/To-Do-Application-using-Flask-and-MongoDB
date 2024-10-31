e8689598ef2da241a1cd0cc334f5d08787f22a43
Arsalan Anwar <arsalan.anwar.7777@gmail.com>
2024-10-30 21:26:17 -0400

    Updated app.py

--- app.py
+++ app.py
@@ -124,5 +124,6 @@ if __name__ == "__main__":
 	port = int(os.environ.get('PORT', 5000))
 	debug = False if env == 'production' else True
 	app.run(debug=True)
-	app.run(port=port, debug=debug)
+	# app.run(port=port, debug=debug)
+	app.run(host='0.0.0.0', port=5000)
 	# Careful with the debug mode..
